b841ff5f9d742bc6b99b40def8dea962a6c8165c
dotNate <nathanielpayne1@googlemail.com>
2022-06-05 16:42:25 +0100

    extended InputField to be usable as a textarea;

--- client/components/inputs/InputField.tsx
+++ client/components/inputs/InputField.tsx
@@ -3,14 +3,15 @@ import { Field } from 'formik';
 import styled from '@emotion/styled';
 
 type Props = {
-    name: string,
-    description?: string,
-    label?: string,
-    error?: string,
-    type?: 'text' | 'email' | 'password' | 'select' | undefined,
+    name: string;
+    description?: string;
+    label?: string;
+    error?: string;
+    type?: 'text' | 'email' | 'password' | 'select' | undefined;
+    textarea?: boolean;
 };
 
-const InputField: React.FC<Props> = ({ name, description, label, error, type }) => {
+const InputField: React.FC<Props> = ({ name, description, label, error, type, textarea }) => {
     const Container = styled.div`
         display: flex;
         flex-direction: column;
@@ -22,6 +23,7 @@ const InputField: React.FC<Props> = ({ name, description, label, error, type }) 
         border-radius: 5px;
         padding: 1rem 1.25rem;
         margin-top: .5rem;
+        max-width: 100%;
         &:focus-visible {
             outline: none;
             border-color: ${error ? 'rgb(215, 55, 55)' : 'rgb(70, 97, 230)'};
@@ -51,6 +53,7 @@ const InputField: React.FC<Props> = ({ name, description, label, error, type }) 
                 id={name} 
                 name={name} 
                 type={type}
+                as={ textarea ? 'textarea' : null }
             />
             {error && <ErrorMessage>{error}</ErrorMessage>}
         </Container>
