224885cd6cbf60edd655dfee1c6ff6be93b8d627
dotNate <nathanielpayne1@googlemail.com>
2022-06-05 16:40:46 +0100

    added SelectField component; implemented into CreateFeedbackInputs;

--- client/components/form/createFeedback/CreateFeedbackInputs.tsx
+++ client/components/form/createFeedback/CreateFeedbackInputs.tsx
@@ -1,10 +1,18 @@
 import InputContainer from "../../inputs/InputContainer";
 import InputField from "../../inputs/InputField";
+import SelectField from "../../inputs/SelectField";
 
 const CreateFeedbackInputs: React.FC = () => {
     return (
         <InputContainer>
             <InputField label='Feedback Title' name='title' type='text' description='Add a short, descriptive headline.' />
+            <SelectField label='Feedback Title' name='category' description='Add a short, descriptive headline.'>
+                <option value="">Please choose an option...</option>
+                <option value="feature">Feature</option>
+                <option value="enhancement">Enhancement</option>
+                <option value="bug">Bug</option>
+            </SelectField>
+            <InputField label='Feedback Detail' name='detail' textarea={true} description='Include any specific comments on what should be improved, added, etc.' />
         </InputContainer>
     );
 };
---
+++ client/components/inputs/SelectField.tsx
@@ -0,0 +1,73 @@
+import React, { ReactNode } from 'react';
+import { Field } from 'formik';
+import styled from '@emotion/styled';
+
+type Props = {
+    name: string;
+    description?: string;
+    label?: string;
+    error?: string;
+    children: ReactNode;
+};
+
+const SelectField: React.FC<Props> = ({ name, description, label, error, children }) => {
+    const Container = styled.div`
+        display: flex;
+        flex-direction: column;
+    `;
+    
+    const SelectWrapper = styled.div`
+        display: flex;
+        background-color: rgb(247, 248, 253);
+        border: 1px solid ${error ? 'rgb(215, 55, 55)' : 'transparent'};
+        border-radius: 5px;
+        padding: 1rem 1.25rem;
+        margin-top: .5rem;
+        max-width: 100%;
+        &:focus-within {
+            border-color: ${error ? 'rgb(215, 55, 55)' : 'rgb(70, 97, 230)'};
+        }
+    `;
+
+    const Select = styled(Field)`
+        border: 1px solid transparent;
+        background-color: transparent;
+        width: 100%;
+        &:focus-visible {
+            outline: none;
+        }
+    `;
+
+    const Description = styled.p`
+        font-size: 14px;
+        color: rgb(100, 113, 150);
+    `;
+
+    const ErrorMessage = styled.p`
+        color: rgb(215, 55, 55);
+        font-size: 14px;
+        margin-top: .25rem;
+    `;
+
+    return (
+        <Container>
+            {label && 
+                <label htmlFor={name}>
+                    <h4>{label}</h4>
+                </label>
+            }
+            {description && <Description>{description}</Description>}
+            <SelectWrapper>
+                <Select
+                    id={name} 
+                    name={name} 
+                    as='select'
+                >
+                    { children }
+                </Select>
+            </SelectWrapper>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+        </Container>
+    );
+};
+export default SelectField;
