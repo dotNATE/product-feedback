43528099ed58fbce440857374bb5ab2e2f22b8e2
dotNate <nathanielpayne1@googlemail.com>
2022-06-05 14:30:03 +0100

    added some styles to global and Feedback;

--- client/views/Home/components/Feedback.tsx
+++ client/views/Home/components/Feedback.tsx
@@ -12,17 +12,42 @@ type Props = {
 };
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: column;
     background-color: white;
     border-radius: 10px;
     padding: 1.75rem;
+    align-items: flex-start;
+    gap: .275rem;
 `;
+
+const Detail = styled.p`
+    color: rgb(100, 113, 150);
+`
+
+const CategoryPill = styled.p`
+    background-color: rgb(242, 244, 255);
+    color: rgb(70, 97, 230);
+    padding: 0.6875rem 1.25rem;
+    border-radius: 10px;
+`
+
+const toSentenceCase = (string: string): string => {
+    let result: string = '';
+
+    const stringArray = string.split('');
+    stringArray[0] = stringArray[0].toUpperCase();
+    result = stringArray.join('');
+
+    return result;
+};
 
 const Feedback: React.FC<Props> = ({ feedback }) => {
     return (
         <Container>
             <h3>{ feedback.title }</h3>
-            <p>{ feedback.detail }</p>
-            <p>{ feedback.category }</p>
+            <Detail>{ feedback.detail }</Detail>
+            <CategoryPill className='two'>{ toSentenceCase(feedback.category) }</CategoryPill>
         </Container>
     );
 };
