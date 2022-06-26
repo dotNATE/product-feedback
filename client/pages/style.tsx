8f3ada558ea99b6a18f348f9774b4348412c1a8d
dotNate <nathanielpayne1@googlemail.com>
2022-06-26 08:47:18 +0100

    added /style dev route for ui

---
+++ client/pages/style.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+import Style from '../views/Style';
+
+const StylePage: NextPage = () => {
+  return (
+    <Style />
+  )
+};
+
+export default StylePage;
---
+++ client/views/Style/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Style: React.FC = ({}) => {
+    return (
+        <div>STYLE</div>
+    );
+};
+
+export default Style;
