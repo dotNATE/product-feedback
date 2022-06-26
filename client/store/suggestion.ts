13fd4fe649b534da1dabe7964248c18cc18fda19
dotNate <nathanielpayne1@googlemail.com>
2022-06-26 13:51:28 +0100

    + suggestionSortLabel to sotre/suggestion

--- client/store/suggestion.ts
+++ client/store/suggestion.ts
@@ -7,6 +7,7 @@ export type SuggestionState = {
     suggestions: SuggestionType[],
     suggestionFilter: string,
     suggestionSort: (a: SuggestionType, b: SuggestionType) => number,
+    suggestionSortLabel: string,
     createSuggestionOpen: boolean;
 };
 
@@ -14,6 +15,7 @@ const initialState: SuggestionState = {
     suggestions: [],
     suggestionFilter: 'all',
     suggestionSort: (a: SuggestionType, b: SuggestionType) => b.upvotes - a.upvotes,
+    suggestionSortLabel: 'Most Upvotes',
     createSuggestionOpen: false,
 };
 
@@ -34,7 +36,8 @@ export const suggestionSlice = createSlice({
             state.createSuggestionOpen = false;
         },
         setSuggestionSort: (state, { payload }) => {
-            state.suggestions.sort(payload);
+            state.suggestionSort = payload.sort;
+            state.suggestionSortLabel = payload.label;
         },
     },
 });
@@ -43,5 +46,6 @@ export const { addSuggestionsToState, setSuggestionFilter, openCreateSuggestion,
 export const selectSuggestions = (state: RootState) => state.suggestion.suggestions;
 export const selectSuggestionFilter = (state: RootState) => state.suggestion.suggestionFilter;
 export const selectSuggestionSort = (state: RootState) => state.suggestion.suggestionSort;
+export const selectSuggestionSortLabel = (state: RootState) => state.suggestion.suggestionSortLabel;
 export const selectCreateSuggestion = (state: RootState) => state.suggestion.createSuggestionOpen;
 export default suggestionSlice.reducer;
