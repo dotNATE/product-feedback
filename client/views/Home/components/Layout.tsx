3293ca91524a3048959a61b0e23c9a408a37bbf0
dotNATE <56537579+dotNATE@users.noreply.github.com>
2022-07-03 19:24:58 +0100

    refactored Modal and Grid implementation in Home/Layout to use mui components

--- client/views/Home/components/Layout.tsx
+++ client/views/Home/components/Layout.tsx
@@ -1,44 +1,49 @@
 import React, { ReactNode } from 'react';
-import styled from '@emotion/styled';
 
-type Props = {
+import { Dialog, Grid } from '@mui/material';
+
+import { useAppDispatch } from '@store/hooks';
+import { closeCreateSuggestion } from '@store/suggestion';
+
+interface Props {
     primaryColumnContent: ReactNode,
     secondaryColumnContent: ReactNode,
     modal?: ReactNode,
 }
 
-const Layout: React.FC<Props> = ({ primaryColumnContent , secondaryColumnContent, modal }) => {
-    const SecondaryColumn = styled.div`
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-        width: 100%;
-    `;
+const styles = {
+    grid: {
+        padding: '5.875rem 10.3125rem 0 10.3125rem',
+    },
+    dialog: {
+        padding: '3.5rem 4rem',
+    },
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+        width: '100%',
+    },
+};
 
-    const PrimaryColumn = styled.div`
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-        width: 100%;
-        grid-column: 2 / span 3;
-    `;
+const Layout: React.FC<Props> = ({ primaryColumnContent , secondaryColumnContent, modal }) => {  
+    const dispatch = useAppDispatch();
 
-    const Grid = styled.div`
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-column-gap: 2rem;
-        padding: 5.875rem 10.3125rem 0 10.3125rem;
-    `;
+    const handleCloseModal = () => {
+        dispatch(closeCreateSuggestion())
+    };
 
     return (
-        <Grid>
-            <SecondaryColumn>
+        <Grid container columns={{ xs: 4 }} spacing={{ xs: 4 }} sx={ styles.grid }>
+            <Grid item xs={1} sx={ styles.column }>
                 { secondaryColumnContent }
-            </SecondaryColumn>
-            <PrimaryColumn>
+            </Grid>
+            <Grid item xs={3} sx={ styles.column }>
                 { primaryColumnContent }
-            </PrimaryColumn>
-            { modal }
+            </Grid>
+            <Dialog open={ !!modal } sx={ styles.dialog } onClose={ handleCloseModal }>
+                { modal }
+            </Dialog>
         </Grid>
     );
 };
